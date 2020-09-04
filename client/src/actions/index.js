2cda86df5ae9b310a0ece101ef43a5ffe58c634f
Katy Herrick <kmh530@nyu.edu>
2020-09-04 17:05:19 -0400

    add all CRUD actions for app

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+	SIGN_IN,
+	SIGN_OUT,
+	CREATE_STREAM,
+	READ_STREAM,
+	READ_STREAMS,
+	UPDATE_STREAM,
+	DELETE_STREAM
+} from './types';
 
 export const signIn = (userId) => {
 	return {
@@ -15,5 +23,26 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-	streams.post('/streams', formValues);
+	const response = await streams.post('/streams', formValues);
+	dispatch({ type: CREATE_STREAM, payload: response.data });
+}
+
+export const readStream = (id) => async dispatch => {
+	const response = await streams.get(`/streams/${id}`);
+	dispatch({ type: READ_STREAM, payload: response.data });
+}
+
+export const readStreams = () => async dispatch => {
+	const response = await streams.get('/streams');
+	dispatch({ type: READ_STREAMS, payload: response.data });
+}
+
+export const updateStream = (id, formValues) => async dispatch => {
+	const response = await streams.put(`/streams/${id}`, formValues);
+	dispatch({ type: UPDATE_STREAM, payload: response.data });
+}
+
+export const deleteStream = (id) => async dispatch => {
+	await streams.delete(`/streams/${id}`);
+	dispatch({ type: DELETE_STREAM, payload: id });
 }
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,7 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
+export const CREATE_STREAM = 'CREATE_STREAM';
+export const READ_STREAM = 'READ_STREAM';
+export const READ_STREAMS = 'READ_STREAMS';
+export const UPDATE_STREAM = 'UPDATE_STREAM';
+export const DELETE_STREAM = 'DELETE_STREAM';
