c835288852ba7e5f3d759e0bfdca7ab127d05a01
Katy Herrick <kmh530@nyu.edu>
2020-09-04 14:08:04 -0400

    add redux form and set up inputs on create screen

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-	return (
-		<div>
-			StreamCreate
-		</div>
-	);
-};
+class StreamCreate extends React.Component {
+	// Destructure input out of reduxForm's Field formProps
+	renderInput({ input, label }) {
+		// Syntactical sugar to pass all of the props down
+		return (
+			<div className="field">
+				<label>{label}</label>
+				<input {...input} />
+			</div>
+		);
+	}
 
-export default StreamCreate;
+	onSubmit(formValues) {
+		console.log(formValues);
+	}
+
+	render() {
+		return (
+			// redux-form provides handleSubmit(fn) through props.
+			<form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+				<Field name="title" label="Enter Title" component={this.renderInput} />
+				<Field
+					label="Enter Description"
+					name="description"
+					component={this.renderInput}
+				/>
+				<button className="ui button primary">Submit</button>
+			</form>
+		);
+	}
+}
+
+export default reduxForm({
+	form: 'streamCreate'
+})(StreamCreate);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
-	auth: authReducer
+	auth: authReducer,
+	// form is a special key required by redux-form
+	form: formReducer
 });
