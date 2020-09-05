2c94284063d09c05a086c9f1e580f2da47b51056
Katy Herrick <kmh530@nyu.edu>
2020-09-04 21:04:23 -0400

    show streams on homepage

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { readStreams } from '../../actions';
 
-const StreamList = () => {
-	return (
-		<div>
-			StreamList
-		</div>
-	);
+class StreamList extends React.Component {
+	componentDidMount() {
+		this.props.readStreams();
+	}
+
+	renderStreams() {
+		return this.props.streams.map(stream => {
+			return (
+				<div className="item" key={stream.id}>
+					<i className="large middle aligned icon camera" />
+					<div className="content">
+						{stream.title}
+						<div className="description">{stream.description}</div>
+					</div>
+				</div>
+			);
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Streams</h2>
+				<div className="ui celled list">{this.renderStreams()}</div>
+			</div>
+		);
+	}
 };
 
-export default StreamList;
+const mapStateToProps = (state) => {
+	return { streams: Object.values(state.streams) };
+};
+
+export default connect(
+	mapStateToProps,
+	{ readStreams }
+)(StreamList);
