23e81812189afe6909eebe0c5c9c2604a8f4ff16
Katy Herrick <kmh530@nyu.edu>
2020-09-04 21:26:45 -0400

    add create/delete buttons to user-owned streams and add link to new stream page

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { readStreams } from '../../actions';
 
@@ -11,6 +12,7 @@ class StreamList extends React.Component {
 		return this.props.streams.map(stream => {
 			return (
 				<div className="item" key={stream.id}>
+					{this.renderAdminButtons(stream)}
 					<i className="large middle aligned icon camera" />
 					<div className="content">
 						{stream.title}
@@ -21,18 +23,47 @@ class StreamList extends React.Component {
 		});
 	}
 
+	renderAdminButtons(stream) {
+		if (stream.userId === this.props.currentUserId) {
+			return (
+				<div className="right floated content">
+					<button className="ui button primary">
+						Edit
+					</button>
+					<button className="ui button negative">
+						Delete
+					</button>
+				</div>
+			);
+		}
+	}
+
+	renderCreateButton() {
+		if (this.props.isSignedIn) {
+			return (
+				<div style={{ textAlign: 'right' }}>
+					<Link to="/streams/new" className="ui button primary">Create Stream</Link>
+				</div>
+			);
+		}
+	}
+
 	render() {
 		return (
 			<div>
 				<h2>Streams</h2>
 				<div className="ui celled list">{this.renderStreams()}</div>
+				{this.renderCreateButton()}
 			</div>
 		);
 	}
 };
 
 const mapStateToProps = (state) => {
-	return { streams: Object.values(state.streams) };
+	return {
+		isSignedIn: state.auth.isSignedIn,
+		currentUserId: state.auth.userId,
+		streams: Object.values(state.streams) };
 };
 
 export default connect(
