206a6a530a57e78b5d4fa511efb5ab6871909247
Katy Herrick <kmh530@nyu.edu>
2020-09-06 17:42:29 -0400

    refactor StreamShow page into a class component

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { readStream } from '../../actions';
 
-const StreamShow = () => {
-	return (
-		<div>
-			StreamShow
-		</div>
-	);
+class StreamShow extends React.Component {
+	componentDidMount()  {
+		this.props.readStream(this.props.match.params.id);
+	}
+
+	render() {
+		return (
+			<div>
+				StreamShow {this.props.match.params.id}
+			</div>
+		);
+	}
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+	return {
+		stream: state.streams[ownProps.match.params.id]
+	}
+};
+
+export default connect(
+	null,
+	{ readStream }
+)(StreamShow);
