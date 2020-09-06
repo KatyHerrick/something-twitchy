c42b8951b7eeec1f952d1ffa1bebee6af478d1c4
Katy Herrick <kmh530@nyu.edu>
2020-09-05 20:47:57 -0400

    refactor modal to be reusable and use modal on delete page

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
+	const actions = (
+		<>
+			<button
+				className="ui button negative"
+			>
+				Delete
+			</button>
+			<button
+				className="ui button"
+			>
+				Cancel
+			</button>
+		</>
+	);
+
 	return (
 		<div>
 			StreamDelete
-			<Modal />
+			<Modal
+				title="Delete Stream"
+				content="Are you sure you want to delete this stream?"
+				actions={actions}
+				onDismiss={() => history.push('/')}
+			/>
 		</div>
 	);
 };
